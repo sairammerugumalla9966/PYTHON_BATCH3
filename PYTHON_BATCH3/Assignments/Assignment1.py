b7e0f03e6da695bb602102615b4e7be46b252a26
keerthisri-ks <keerthisri.k@atenas.in>
2026-03-01 23:22:13 +0530

    Update Assignment1.py

--- PYTHON_BATCH3/Assignments/Assignment1.py
+++ PYTHON_BATCH3/Assignments/Assignment1.py
@@ -1,51 +1,51 @@
-''' import math
+import math
 radius = float(input("Enter Radius of Circle:"))
 area = math.pi*radius*radius
-print(area) '''
+print(area) 
 
-''' originalprice = float(input("Enter the Original Price:"))
+originalprice = float(input("Enter the Original Price:"))
 discountpercentage = float(input("Enter the Discount Percentage:"))
 discountamount = (originalprice * discountpercentage)/100
 finalprice = originalprice - discountamount
-print(finalprice) '''
+print(finalprice) 
 
-'''incipal=float(input("Enter principal amount:"))
+incipal=float(input("Enter principal amount:"))
 rate=float(input("Enter rate of interest:"))
 time=float(input("Enter time (years):"))
 simpleinterest = principal*rate*time/100
-print(simpleinterest)'''
+print(simpleinterest)
 
-'''basicsalary = float(input("Enter basic salary:"))
+basicsalary = float(input("Enter basic salary:"))
 hra = float(input("Enter hra:"))
 da = float(input("Enter da:"))
 totalsalary = basicsalary+hra+da
-print(totalsalary)'''
+print(totalsalary)
 
-'''speed = float(input("Enter speed:"))
+speed = float(input("Enter speed:"))
 time = float(input("Enter time:"))
 distance = speed*time
-print(distance)'''
+print(distance)
 
-'''celsius = float(input("enter temperature in celsius:"))
+celsius = float(input("enter temperature in celsius:"))
 fahrenheit = (celsius*9/5)+32
-print(fahrenheit)'''
+print(fahrenheit)
 
-'''a = float(input("Enter the first number:"))
+a = float(input("Enter the first number:"))
 b = float(input("Enter the second number:"))
 maximum = a if a>b else b
-print(maximum)'''
+print(maximum)
 
-'''a = int(input("Enter first number:"))
+a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 a = a+b
 b = a-b
 a = a-b
 print("a",a)
-print("b",b)'''
+print("b",b)
 
-'''n = int(input("Enter a number:"))
+n = int(input("Enter a number:"))
 nextmultiple = ((n // 100) + 1) * 100
-print("Next multiple of 100 is:", nextmultiple)'''
+print("Next multiple of 100 is:", nextmultiple)
 
 people = int(input("Enter number of people:"))
 teams = people // 2
