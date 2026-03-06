21e256ef322c2141f28440df3c386657ed1228a1
sairammerugumalla9966 <sairammerugumalla@gmail.com>
2026-03-06 13:34:48 +0530

    all files updated till Day11

---
+++ Day11/functions.py
@@ -0,0 +1,72 @@
+# Function : it is a reuseable block of code which is used for performing particular task .
+
+# uses :
+# reduce duplicate code 
+# reuse code 
+# easy maintaince 
+# modularity 
+
+'''
+
+def function_name(): # definition 
+    statements 
+    statements 
+    statements 
+    statements
+    statements 
+
+
+function_name() # function calling
+
+
+# snake_case naming convention 
+# CamelCase naming convention 
+
+'''
+'''
+def sample_function():
+    print("function defined successfully")
+
+
+sample_function()
+
+print("outside function")
+
+'''
+def addition():
+    a=10
+    b=20
+    c=a+b
+
+add1=addition()
+print(add1)
+
+
+def addition():
+    a=10
+    b=20
+    c=a+b
+    return c
+
+add2=addition()
+print(add2)
+
+
+
+# functions with parameters 
+
+#def function_name(parmeters):
+    #statements / functionbody
+
+
+#function_name(arguments)
+
+
+
+def addition1(num1, num2):
+    return num1 + num2
+
+
+print(addition1(70,30))
+
+
